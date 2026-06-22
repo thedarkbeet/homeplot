94adf6cc37af19988ad55125c403d7aff4fdfca8
Jophkins <sergebrew@gmail.com>
2026-06-21 20:32:29 -0600

    :sparkles: feat: learn and test loading.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  await new Promise(resolve => setTimeout(resolve, 2000)); // Simulate a delay
+
   return (
     <div className="p-4 bg-emerald-50 border-2 border-emerald-500 rounded-md">
       <h1 className="text-xl font-bold text-black">
