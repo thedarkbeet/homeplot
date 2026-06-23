3320cdd7204ecdcfabca296b8d98a885dbbd28bf
Jophkins <sergebrew@gmail.com>
2026-06-22 20:15:42 -0600

    :sparkles: feat: add error page and not-found page

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 
 export default function Settings() {
+  // const data = null;
+
+  // if (!data)
+  //   notFound();
+
   return (
     <div className="p-4 bg-zinc-50 border-2 border-zinc-500 rounded-md">
       <h1 className="text-xl font-bold text-black">
---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+  return (
+    <div className="p-4 border-blue-500 bg-blue-50 rounded-lg text-blue-700">
+      <h2 className="font-bold text-lg">
+        Not Found
+      </h2>
+      <p className="text-sm my-2 font-mono">
+        The page you are looking for does not exist.
+      </p>
+    </div>
+  );
+}
